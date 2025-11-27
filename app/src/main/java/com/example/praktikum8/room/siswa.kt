e8ef4a825eb78afca506634605d775b2ab29a8c7
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-11-27 11:30:06 +0700

    Tambahkan Room Entity Siswa

--- app/src/main/java/com/example/praktikum8/room/siswa.kt
+++ app/src/main/java/com/example/praktikum8/room/siswa.kt
@@ -3,11 +3,11 @@ package com.example.praktikum8.room
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "siswa")
+@Entity(tableName = "tblsiswa")
 data class Siswa(
-        @PrimaryKey(autoGenerate = true)
-        val id: Int = 0,
-        val nama : String,
-        val alamat : String,
-        val telpon:String
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon:String
 )
