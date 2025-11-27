83793e0473cfccb51fb730f9c303b03bd750b73b
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-11-27 11:33:19 +0700

    Siapkan Kontainer Aplikasi untuk Dependensi Room

---
+++ app/src/main/java/com/example/praktikum8/repositori/ContainerApp.kt
@@ -0,0 +1,19 @@
+package com.example.praktikum8.repositori
+
+
+import android.app.Application
+import android.content.Context
+import com.example.praktikum8.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
