3eb04d29ade5ca153fc9192caaf09963e34a4778
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-11-27 16:01:39 +0700

    Implement navigation host and routes for Home and Entry screens

---
+++ app/src/main/java/com/example/praktikum8/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,44 @@
+package com.example.praktikum8.view.uicontroller
+
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.internal.composableLambda
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+//import com.example.praktikum8.DestinasiEntry
+import com.example.praktikum8.view.route.DestinasiEntry
+//import com.example.praktikum8.view.route.DestinasiEntry
+import com.example.praktikum8.view.route.DestinasiHome
+
+
+
+
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier = Modifier) {
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = modifier) {
+        composable(route = DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(route = DestinasiEntry.route) },
+            )
+        }
+        composable(route = DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
+}
+
+
