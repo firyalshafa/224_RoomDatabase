9a8104f33847ebeef4f61b4c07ad92db4f7a7e40
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-11-27 15:56:15 +0700

    Add reusable SiswaTopAppBar component with back navigation support

---
+++ app/src/main/java/com/example/praktikum8/view/route/DestinasiEntry.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum8.view.route
+
+import com.example.praktikum8.R
+
+
+object DestinasiEntry: DestinasiNavigasi {
+  override val route = "item_entry"
+  // Ganti string literal dengan R.string.ID_STRING yang Anda definisikan
+  override val titleRes = R.string.entry_siswa // Asumsi: R.string.entry_siswa adalah "Tambah Siswa"
+}
