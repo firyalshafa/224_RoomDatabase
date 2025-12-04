950e0d1c44a7c7bf244f04bb2982b8d8c6b7437d
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-04 16:20:37 +0700

    Tambahkan objek DestinasiEntry untuk navigasi input data

--- app/src/main/java/com/example/praktikum8/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/praktikum8/view/route/DestinasiEntry.kt
@@ -5,6 +5,5 @@ import com.example.praktikum8.R
 
 object DestinasiEntry: DestinasiNavigasi {
   override val route = "item_entry"
-  // Ganti string literal dengan R.string.ID_STRING yang Anda definisikan
-  override val titleRes = R.string.entry_siswa // Asumsi: R.string.entry_siswa adalah "Tambah Siswa"
+  override val titleRes = R.string.entry_siswa
 }
