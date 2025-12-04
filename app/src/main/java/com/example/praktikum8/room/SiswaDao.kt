0f015f3376e861909aaa94684c5e238b5a8a368d
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-04 16:18:54 +0700

    Implementasi SiswaDao untuk CRUD

--- app/src/main/java/com/example/praktikum8/room/SiswaDao.kt
+++ app/src/main/java/com/example/praktikum8/room/SiswaDao.kt
@@ -1,10 +1,14 @@
 package com.example.praktikum8.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
+
+
 
 @Dao
 interface SiswaDao {
@@ -12,8 +16,14 @@ interface SiswaDao {
     fun getAllSiswa() : Flow<List<Siswa>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(siswa:Siswa)
+    suspend fun insert(siswa: Siswa)
 
-    @Query(value = "SELECT * from tblsiswa where id  = :id")
-    fun getSiswa(id: Int): Flow<Siswa>
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa?>
+
+    @Update
+    suspend fun update(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
 }
