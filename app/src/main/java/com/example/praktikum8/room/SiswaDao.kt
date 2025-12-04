e89263cfc3badcc2aade6534a5e54667b1a0d7d9
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-04 09:17:27 +0700

    Add getSiswa query to retrieve Siswa data by ID from database

--- app/src/main/java/com/example/praktikum8/room/SiswaDao.kt
+++ app/src/main/java/com/example/praktikum8/room/SiswaDao.kt
@@ -13,4 +13,7 @@ interface SiswaDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa:Siswa)
+
+    @Query(value = "SELECT * from tblsiswa where id  = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
 }
