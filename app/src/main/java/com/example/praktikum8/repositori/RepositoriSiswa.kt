8ac2f1824057eaeac0852becce07cc2baafd7876
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-04 09:03:42 +0700

    Implement getSiswaStream method to retrieve Siswa data by ID

--- app/src/main/java/com/example/praktikum8/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/praktikum8/repositori/RepositoriSiswa.kt
@@ -6,6 +6,9 @@ import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
+    fun getSiswaStream(id:int ):flow<Siswa?>
+
+
 
     suspend fun insertSiswa(siswa: Siswa)  // Fixed the syntax error here
 }
@@ -16,4 +19,6 @@ class OfflineRepositoriSiswa(
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
 
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    override fun getSiswaStream(id: int): flow<Siswa?> = SiswaDao.getSiswa(id)
 }
