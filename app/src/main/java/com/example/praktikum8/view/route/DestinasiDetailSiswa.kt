605f410499024672679daee45f3dcc56c38fd4ce
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-04 16:19:35 +0700

    Tambahkan objek DestinasiDetailSiswa untuk navigasi

---
+++ app/src/main/java/com/example/praktikum8/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum8.view.route
+
+import com.example.praktikum8.R
+
+object DestinasiDetailSiswa :DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
