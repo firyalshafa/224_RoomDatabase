0d60c23ff55180d9952855b21a8bdf5df349a8b9
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-04 09:41:35 +0700

    Fix missing and undefined parameters in HomeScreen navigation

--- app/src/main/java/com/example/praktikum8/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/praktikum8/view/uicontroller/PetaNavigasi.kt
@@ -1,27 +1,28 @@
 package com.example.praktikum8.view.uicontroller
 
 
+import android.R.attr.type
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.internal.composableLambda
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.praktikum8.view.route.DestinasiDetailSiswa
+import com.example.praktikum8.view.route.DestinasiDetailSiswa.itemIdArg
 //import com.example.praktikum8.DestinasiEntry
 import com.example.praktikum8.view.route.DestinasiEntry
 //import com.example.praktikum8.view.route.DestinasiEntry
 import com.example.praktikum8.view.route.DestinasiHome
 
-
-
 @Composable
 fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
     HostNavigasi(navController = navController)
 }
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -41,8 +42,18 @@ fun HostNavigasi(
             EntrySiswaScreen(
                 navigateBack = { navController.popBackStack() }
             )
-
-        }}}
+        }
+        composable(route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(name = itemIdArg) {
+                type = NavType.IntType
+            })
+        ){
+            DetailSiswaScreen(
+                navigateBack = {navController.navigateUp()}
+            )
+        }
+    }
+}
 
 
 
